b18094d8300826281cf245db3caedd9899a855eb
abdias <abdias@alumnos.uaslp.edu.mx>
2023-04-17 00:45:42 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+ session_start();
+ require("SimpleAuth.php");
+ $login = new Login;
+ $login->authorize();
+ ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml">  <head> <title>Impresion de Voucher</title>  <link href="https://fonts.cdnfonts.com/css/source-code-pro" rel="stylesheet"> <style type="text/css">     table { page-break-inside:auto }     tr    { page-break-inside:avoid; page-break-after:auto }     thead { display:table-header-group }     tfoot { display:table-footer-group }  @media print {  .noprint { display: none; }   @page {      size: auto;     margin: 15mm 2mm 0mm 6mm; } body {     margin:0;     padding:0; } }    </style>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
     </head>  <body>
@@ -8,6 +14,7 @@
                 <div class="card mt-4">
                     <div class="card-header">
                         <h4>Imprimir Vouchers desde nombre de Lotes (batch_name) </h4>
+                        <a href="index.php?action=clear_login">Desconectarme</a>
                     </div>
                     <div class="card-body">
                         <div class="row">
@@ -34,7 +41,7 @@
                                     if(isset($_GET['search']))
                                     {
                                         $filtervalues = $_GET['search'];
-                    $query = "SELECT * FROM radius.fichas WHERE CONCAT(batch_name) LIKE '%$filtervalues%'";
+                    $query = "SELECT * FROM radius.fichas WHERE CONCAT(batch_name) LIKE '$filtervalues'";
                                         $query_run = mysqli_query($con, $query);
 
                                         if(mysqli_num_rows($query_run) > 0)
@@ -73,7 +80,7 @@
                                         {
                                             ?>
                                                 <tr>
-                                                    <td colspan="4">No Record Found</td>
+                                                    <td colspan="4">No se encontro nada alli</td>
                                                 </tr>
                                             <?php
                                         }
